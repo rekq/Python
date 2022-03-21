2e61278afc668240bfc6b88dd2034bad3dd82149
rekq <patryk717@gmail.com>
2022-03-21 20:55:50 +0100

    Add files via upload

---
+++ Day 2/main.py
@@ -0,0 +1,9 @@
+print("Welcome to the tip calculator!")
+a = float(input("What was the total bill? $"))
+b = int(input("How much tip would you like to give? 10, 12 or 15? "))
+c = int(input("How many people to split the bill? "))
+
+result = ((a * (b / 100)) + a) / c
+final = "{0:.2f}".format(result)
+message = f"Each person should pay: ${final}"
+print(message)
