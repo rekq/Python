a5a1f34728e7b123f78579a4bdd1b1dfbe6201df
rekq <patryk717@gmail.com>
2022-03-21 20:57:40 +0100

    Update main.py

--- Day 2/main.py
+++ Day 2/main.py
@@ -1,4 +1,5 @@
 print("Welcome to the tip calculator!")
+
 a = float(input("What was the total bill? $"))
 b = int(input("How much tip would you like to give? 10, 12 or 15? "))
 c = int(input("How many people to split the bill? "))
